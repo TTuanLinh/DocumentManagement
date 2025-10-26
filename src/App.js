edc48343ffc18b7434ccac36d7e89e5aee613087
TuanLinh <trumankhoc247@gmail.com>
2025-10-26 18:05:34 +0700

    Update app.js to contain all the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Router, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './App.css';
 
 import SideBar from "./components/SideBar/SideBar";
 import HomePage from "./pages/HomePage/HomePage";
 import JustAnotherPage from "./pages/HomePage/JustAnotherPage";
+import AuthHeader from "./components/AuthHeader/AuthHeader";
+import RightColumn from "./components/RightColumn/RightColumn";
 
 function App(){
   return(
     <BrowserRouter>
-      <div className="appWrapper">
-        <SideBar />
-        <main className="mainContent">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/homepage" element={<HomePage />} />
-            <Route path="/justanotherpage" element={<JustAnotherPage />} />
-          </Routes>
-        </main>
+      <div className="rootLayout">
+        <AuthHeader></AuthHeader>
+        <div className="appWrapper">
+          <SideBar></SideBar>
+          <main className="mainContent">
+            <Routes>
+              <Route path="/" element={<HomePage></HomePage>} />
+              <Route path="/homepage" element={<HomePage></HomePage>} />
+              <Route path="/justanotherpage" element={<JustAnotherPage></JustAnotherPage>} />
+            </Routes>
+          </main>
+          <RightColumn></RightColumn>
+        </div>
       </div>
     </BrowserRouter>
   )
